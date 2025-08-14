1d99902e3755d947747eeab4daaeb52b238a70d6
furkanturunc <furkanturunc@furkan-mbp.local>
2025-08-14 10:58:55 +0300

    Add snake-game

---
+++ snake-game/main.py
@@ -0,0 +1,61 @@
+from idlelib.zoomheight import set_window_geometry
+from turtle import Screen
+import time
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+# setup screen size, background color, and title
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+# create snake and put onto screen
+snake = Snake()
+snake.head.speed(1000)
+
+# create food
+food = Food()
+
+# create scoreboard
+scoreboard = Scoreboard()
+
+screen.listen()
+is_game_on = True
+
+screen.onkey(key = "Up", fun = snake.up)
+screen.onkey(key= "Down", fun=snake.down)
+screen.onkey(key= "Left", fun=snake.left)
+screen.onkey(key= "Right", fun=snake.right)
+
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+    # Check if snake eats food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # check if snake hits wall
+    if abs(snake.head.xcor()) > 280 or abs(snake.head.ycor()) > 280:
+        is_game_on = False
+        scoreboard.game_over()
+
+    # check if snake head hits any of the other segments:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            is_game_on = False
+            scoreboard.game_over()
+            break
+
+screen.exitonclick()
+
+
+
+
+
---
+++ snake-game/scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+FONT = ('Courier', 24, 'normal')
+ALIGN = "center"
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.score = 0
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(arg=f"Score: {self.score}", align=ALIGN, font=FONT)
+
+    def increase_score(self):
+        self.clear()
+        self.score += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(arg="GAME OVER", align=ALIGN, font=FONT)
